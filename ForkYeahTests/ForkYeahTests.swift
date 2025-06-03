9571db9cd8830c1814a17ccdb61c7f5dae6baace
Farshad Sheykhi <farshad@Farshads-Mac-Studio.local>
2025-06-03 13:59:47 -0700

    first commit

---
+++ ForkYeahTests/ForkYeahTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForkYeahTests.swift
+//  ForkYeahTests
+//
+//  Created by Farshad Sheykhi on 6/3/25.
+//
+
+import Testing
+@testable import ForkYeah
+
+struct ForkYeahTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
